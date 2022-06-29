a90bd6269c191b94e5d89af761b8f2981c73b5fa
WaterBongo <cutegato@riseup.net>
2022-06-29 02:15:48 -0700

    Update StreakBased.py

--- StreakBased.py
+++ StreakBased.py
@@ -143,7 +143,7 @@ class main:
 		print("[", end="")
 		cprint(base64.b64decode(b'IENSRURJVFMg').decode('utf-8'), "cyan", end="")
 		print("]", end="")
-		print(base64.b64decode(b'IE1hZGUgYnkgSWNlIEJlYXIjMDE2Nw==').decode('utf-8'))
+		print(base64.b64decode(b'IE1hZGUgYnkgSWNlIEJlYXIjMDE2NyAmIEN1dGVjYXQgYnV0IHRlcm1lZCM0NzI4').decode('utf-8'))
 		time.sleep(3)
 		self.clear()
 
