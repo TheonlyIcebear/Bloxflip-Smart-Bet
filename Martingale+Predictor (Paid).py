9742076b199887fd406853c08dafcf7592dc27f3
TheonlyIcebear <78031685+TheonlyIcebear@users.noreply.github.com>
2022-07-01 08:10:11 -0400

    Add files via upload

--- Martingale+Predictor (Paid).py
+++ Martingale+Predictor (Paid).py
@@ -571,7 +571,7 @@ class main:
 				pass
 			else:
 				sendwebhookmsg(self.webhook, f"Betting {betamount} Robux at {round(prediction,2)}x\n{round(balance-betamount,2)} Robux Left", f"Betting {betamount} Robux ", 0x903cde, f"")
-				sendwebhookmsg(self.webhook,f"Average Crash : {round(avg,2)}\nMultiplier Set to : {multiplier}\n Accuracy on last crash : {, 2)}%","Round Predictions", 0xaf5ebd, f"")
+				sendwebhookmsg(self.webhook,f"Average Crash : {round(avg,2)}\nMultiplier Set to : {multiplier}\n Accuracy on last crash : {accuracy}%","Round Predictions", 0xaf5ebd, f"")
 			try:
 				browser.find_element(By.CSS_SELECTOR, ".MuiButtonBase-root.MuiButton-root.MuiButton-contained.jss142.MuiButton-containedPrimary").click()
 			except:
