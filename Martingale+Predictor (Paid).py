7e8aef3d50bab7b3768681c734a5b61caa62dae8
WaterBongo <cutegato@riseup.net>
2022-06-30 23:07:56 -0700

    readded webbrowser

--- Martingale+Predictor (Paid).py
+++ Martingale+Predictor (Paid).py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python -W ignore::DeprecationWarning
 
-import subprocess, threading, selenium, requests, logging, base64, json, time, os
+import subprocess, threading, selenium, requests, logging, base64, json, time, os, webbrowser
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from win10toast import ToastNotifier
