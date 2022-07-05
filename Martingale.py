9cf230b5cd4d4f126d8e95fb231412adb4a610b1
TheonlyIcebear <78031685+TheonlyIcebear@users.noreply.github.com>
2022-07-05 16:34:00 -0400

    Add files via upload

--- Martingale.py
+++ Martingale.py
@@ -284,7 +284,6 @@ class main:
 			options.add_argument('--profile-directory=Default')
 			options.add_argument("--incognito")
 			options.add_argument("--disable-plugins-discovery")
-			options.add_argument("--headless")
 			options.add_argument("--disable-gpu")
 			options.add_experimental_option("excludeSwitches", ["enable-automation", 'enable-logging'])
 			options.add_experimental_option('useAutomationExtension', False)		
