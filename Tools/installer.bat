784a12ddd94f081fba036bc51a5c59941b8287a8
TheonlyIcebear <78031685+TheonlyIcebear@users.noreply.github.com>
2022-07-09 18:53:13 -0400

    Update installer.bat

--- Tools/installer.bat
+++ Tools/installer.bat
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
-pip install -r requirements.txt
-cls
+python -m pip install -r requirements.txt
 pause
-main.py
-=======
-pip install -r requirements.txt
-pause
->>>>>>> 4269d9b5a7f16395f80d8fdf83e9f15108c40582
