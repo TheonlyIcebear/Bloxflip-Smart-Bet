26ba49f6200a53d4d5653aaf3959fd038bc9e3b0
TheonlyIcebear <78031685+TheonlyIcebear@users.noreply.github.com>
2022-07-12 23:05:37 -0400

    Add files via upload

--- Tools/CrashCalculator.py
+++ Tools/CrashCalculator.py
@@ -9,20 +9,6 @@ def uiprint(message, color="cyan"):
 	cprint(message, color)
 
 
-def getGames(balance):
-	global betamount
-	totalbet = 1
-	count = 0
-	while True:
-		count += 1
-		betamount *= 2
-		totalbet += betamount
-		if totalbet > balance:
-			chance = (1/33 + (32/33)*(.01 + .99*(1 - 1/multiplier)))**count
-			return [chance, count]
-
-
-
 
 while True:
 	os.system("")
@@ -31,7 +17,6 @@ while True:
 	
 
 	uiprint(f"There's a {(1 - (1/33 + (32/33)*(.01 + .99*(1 - 1/multiplier))))*100}% chance of it crashing at or above {multiplier}")
-	
 	print("Press enter to retry")
 	input(">> ")
 	os.system("cls")
