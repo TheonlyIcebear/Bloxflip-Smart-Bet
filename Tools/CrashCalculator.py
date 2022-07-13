10a5789fb81befeac229df4d7b631b7504dac856
TheonlyIcebear <78031685+TheonlyIcebear@users.noreply.github.com>
2022-07-12 23:06:07 -0400

    Add files via upload

--- Tools/CrashCalculator.py
+++ Tools/CrashCalculator.py
@@ -16,7 +16,7 @@ while True:
 	multiplier = float(input(">> "))
 	
 
-	uiprint(f"There's a {(1 - (1/33 + (32/33)*(.01 + .99*(1 - 1/multiplier))))*100}% chance of it crashing at or above {multiplier}")
+	uiprint(f"There's a {(1 - (1/33 + (32/33)*(.01 + .99*(1 - 1/multiplier))))*100}% chance of it crashing at or above {multiplier}x")
 	print("Press enter to retry")
 	input(">> ")
 	os.system("cls")
