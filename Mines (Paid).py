f3d816b2f83cc01d4157e43b3de5ceaa62dbdcee
TheonlyIcebear <78031685+TheonlyIcebear@users.noreply.github.com>
2022-10-23 22:40:14 -0400

    Update Mines (Paid).py

--- Mines (Paid).py
+++ Mines (Paid).py
@@ -17,7 +17,7 @@ class main:
 		logging.basicConfig(filename="errors.txt", level=logging.DEBUG)
 		self.crashPoints = None
 		self.multiplier = 0
-		self.version = "1.31"
+		self.version = "1.32"
 		os.system("")
 		try:
 			self.getConfig()
